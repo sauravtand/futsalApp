995e6edb003c845556d50139d60d9404646e8de1
saro77 <sarowarmalla@gmail.com>
2024-02-16 19:26:19 +0545

    Complete bottom navigation

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text,StyleSheet,Image,TouchableOpacity } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
@@ -17,13 +17,84 @@ const Tab=createBottomTabNavigator();
 export default function MainContainer() {
   return(
     <NavigationContainer> 
-      <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen}/>
-      <Tab.Screen name="Book" component={BookScreen}/>
-      <Tab.Screen name="Play" component={PlayScreen}/>
-      <Tab.Screen name="User" component={ProfileScreen}/>
+      <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel:false,
+        tabBarStyle:{
+          position:'absolute',
+          backgroundColor:'#FEFEFE',
+          bottom:1,
+          height:65,
+        }
+      }}
+      >
+      <Tab.Screen name="Home" component={HomeScreen} options={{
+        tabBarIcon:({focused})=>(
+         <View style={{alignItems:'center',justifyContent:'center',top:10}}>
+           <Image source={require('../assets/pictures/home.png')}
+           resizeMode='contain'
+           style={{
+            width:25,
+            height:25,
+            bottom:5,
+            tintColor:focused? '#08C208':'#748c94'
+           }}
+            />
+           <Text style={{color:focused ? '#08C208':'#748c94',fontSize:16,bottom:5}}>Home</Text>
+         </View>
+        ),
+      }}/>
+      <Tab.Screen name="Book" component={BookScreen} options={{
+        tabBarIcon:({focused})=>(
+         <View style={{alignItems:'center',justifyContent:'center',top:10}}>
+           <Image source={require('../assets/pictures/field.png')}
+           resizeMode='contain'
+           style={{
+            width:25,
+            height:25,
+            bottom:5,
+            tintColor:focused? '#08C208':'#748c94'
+           }}
+            />
+           <Text style={{color:focused ? '#08C208':'#748c94',fontSize:16,bottom:5}}>Book</Text>
+         </View>
+        ),
+      }}/>
+      <Tab.Screen name="Play" component={PlayScreen} options={{
+        tabBarIcon:({focused})=>(
+         <View style={{alignItems:'center',justifyContent:'center',top:10}}>
+           <Image source={require('../assets/pictures/users.png')}
+           resizeMode='contain'
+           style={{
+            width:25,
+            height:25,
+            bottom:5,
+            tintColor:focused? '#08C208':'#748c94'
+           }}
+            />
+           <Text style={{color:focused ? '#08C208':'#748c94',fontSize:16,bottom:5}}>Play</Text>
+         </View>
+        ),
+      }}/>
+      <Tab.Screen name="User" component={ProfileScreen}options={{
+        tabBarIcon:({focused})=>(
+         <View style={{alignItems:'center',justifyContent:'center',top:10}}>
+           <Image source={require('../assets/pictures/circle-user.png')}
+           resizeMode='contain'
+           style={{
+            width:25,
+            height:25,
+            bottom:5,
+            tintColor:focused? '#08C208':'#748c94'
+           }}
+            />
+           <Text style={{color:focused ? '#08C208':'#748c94',fontSize:16,bottom:5}}>User</Text>
+         </View>
+        ),
+      }}/>
     </Tab.Navigator>
     </NavigationContainer>
     
   );
 }
+  
